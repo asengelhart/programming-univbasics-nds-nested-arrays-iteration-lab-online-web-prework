a5a6b5d56ec34e6da715c57317d9150e5b6bbaaf
Alex Engelhart <asengelhart@yahoo.com>
2019-12-02 03:09:05 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -34,7 +34,7 @@ def total_even_pairs(src)
   # this!
   result = []
   src.length.times do |index|
-    if src[index][0] % 2 == 0 && src[index][1] % 2 == 0 do
+    if src[index][0] % 2 == 0 && src[index][1] % 2 == 0
       result << (src[index][0] + src[index][1])
     end 
   end 
